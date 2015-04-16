ca3d031d9ac3ae5c4560c00beb1cc0731b6f8c5a
Goddanao <goddanao@gmail.com>
2015-04-16 08:05:40 +0200

    test ci

---
+++ .travis/ci_build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Build, check, and install libsodium if WITH_LIBSODIUM is set
+if [ -n "$WITH_LIBSODIUM" ]; then
+    git clone git://github.com/jedisct1/libsodium.git &&
+    ( cd libsodium; ./autogen.sh && ./configure &&
+        make check && sudo make install && sudo ldconfig ) || exit 1
+fi
+
+# Build, check, and install the version of ZeroMQ given by ZMQ_REPO
+git clone git://github.com/zeromq/${ZMQ_REPO}.git &&
+( cd ${ZMQ_REPO}; ./autogen.sh && ./configure &&
+    make check && sudo make install && sudo ldconfig ) || exit 1
+
+# Build, check, and install the version of CZMQ given by CZMQ_REPO
+git clone git://github.com/zeromq/${CZMQ_REPO}.git &&
+( cd ${CZMQ_REPO}; ./autogen.sh && ./configure &&
+    make check && sudo make install && sudo ldconfig ) || exit 1
+
+# Build, check, and install the version of ZYRE given by ZYRE_REPO
+git clone git://github.com/zeromq/${ZYRE_REPO}.git &&
+( cd ${ZYRE_REPO}; ./autogen.sh && ./configure &&
+    make check && sudo make install && sudo ldconfig ) || exit 1
+
+# Build, check, and install the version of MALAMUTE given by MALAMUTE_REPO
+git clone git://github.com/zeromq/${MALAMUTE_REPO}.git &&
+( cd ${MALAMUTE_REPO}; ./autogen.sh && ./configure &&
+    make check && sudo make install && sudo ldconfig ) || exit 1
+
+# Build, check, and install PHP-CPP
+
+# ./autogen.sh && ./configure && make check-verbose VERBOSE=1 && sudo make install
