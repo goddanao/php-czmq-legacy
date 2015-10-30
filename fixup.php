9ea67c8d94bbd769a384089f7c77d7d998741906
Goddanao <goddanao@gmail.com>
2015-10-30 11:30:02 +0100

    ZMQ_IPV6 hack for TravisCI problem in zsock_set_ipv6 php: src/zsock_option.c:654: zsock_set_ipv6: Assertion `rc == 0 || zmq_errno () == (156384712 + 53)' failed

--- fixup.php
+++ fixup.php
@@ -6,7 +6,7 @@ $file .= (file_exists($file . "/backup") ? "/vendor" : "");
 $file .= "/czmq/src/zsock_option.c";
 
 $src = file_get_contents($file);
-$src = preg_replace('/(void[\s\r\n]+zsock_set_ipv6.*?)(?:assert \(rc).*?;[\s\r\n]+/sim', '\1', $src);
+$src = preg_replace('/(void[\s\r\n]+zsock_set_ipv6.*?)(?:#.*?(?:assert \(rc).*?;.*?})/sim', '\1}', $src);
 unlink($file);
 file_put_contents($file, $src);
 
