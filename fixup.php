a6ac8aeb34aa5d1cfffc23a634d59dff6967869e
Goddanao <goddanao@gmail.com>
2015-10-30 18:37:21 +0100

    tests added

--- fixup.php
+++ fixup.php
@@ -6,7 +6,7 @@ $file .= (file_exists($file . "/backup") ? "/vendor" : "");
 $file .= "/czmq/src/zsock_option.c";
 
 $src = file_get_contents($file);
-$src = preg_replace('/(void[\s\r\n]+(zsock_set_ipv6|zsock_set_zap_domain).*?{).*?}/sim', "\$1\r\n}", $src);
+$src = preg_replace('/(void[\s\r\n]+(zsock_set_ipv6|zsock_set_zap_domain).*?{.*?assert.*?)(assert.*?})/sim', "\$1zsys_info(\"\$2 -rc=%d -zmq_errno=%d -zmq_errmsg=%s\", rc, zmq_errno(), zmq_strerror(zmq_errno()));\r\n    // \$3",, $src);
 unlink($file);
 file_put_contents($file, $src);
 
