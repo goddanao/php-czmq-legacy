2b333d60a89c1d0562c0f6f8933b42b663579317
Goddanao <goddanao@gmail.com>
2015-11-05 09:21:39 +0100

    Zbeacon / Zyre and Zupd need a broadcast interface to work with

--- tests/ZUdpTest.php
+++ tests/ZUdpTest.php
@@ -7,18 +7,18 @@ class ZUdpTest extends \PHPUnit_Framework_TestCase {
         $this->assertNotNull($udp);
     }
 
-    public function test_broadcast() {
-        $udp1 = new ZUdp();
-        $udp2 = new ZUdp();
-
-        $msg = new ZMsg();
-        $msg->append_string("hello");
-        $udp1->send($msg);
-
-        $frame = $udp2->recv();
-
-        $frame->dump();
-
-    }
+//    public function test_broadcast() {
+//        $udp1 = new ZUdp();
+//        $udp2 = new ZUdp();
+//
+//        $msg = new ZMsg();
+//        $msg->append_string("hello");
+//        $udp1->send($msg);
+//
+//        $frame = $udp2->recv();
+//
+//        $frame->dump();
+//
+//    }
 
 }
