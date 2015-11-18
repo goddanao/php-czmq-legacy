dc1b962773399193c316804bea03fe8f36b22646
Goddanao <goddanao@gmail.com>
2015-11-18 19:43:13 +0100

    ZAuthTest update

--- tests/ZAuthTest.php
+++ tests/ZAuthTest.php
@@ -4,7 +4,6 @@ class ZAuthTest extends \PHPUnit_Framework_TestCase {
 
     private $server;
     private $client;
-    private $auth;
 
     const cert_dir  = "./.certdir";
 
@@ -18,15 +17,18 @@ class ZAuthTest extends \PHPUnit_Framework_TestCase {
 
     public function test_create() {
         $this->reset_sockets();
-        $this->assertNotNull($this->auth);
+        $auth = new ZAuth();
+        $this->assertNotNull($auth);
     }
-/*
+
     public function test_no_auth() {
+        $auth = new ZAuth();
         $this->reset_sockets();
         $this->assertTrue($this->can_connect());
     }
 
     public function test_zap_domain_allow_all() {
+        $auth = new ZAuth();
         $this->reset_sockets();
         $this->server->set_zap_domain("global");
         $this->assertTrue($this->can_connect());
@@ -34,20 +36,26 @@ class ZAuthTest extends \PHPUnit_Framework_TestCase {
 
 
     public function test_zap_domain_deny_localhost() {
+        $auth = new ZAuth();
+
         $this->reset_sockets();
         $this->server->set_zap_domain("global");
-        $this->auth->deny("127.0.0.1");
+        $auth->deny("127.0.0.1");
         $this->assertFalse($this->can_connect());
     }
 
     public function test_zap_domain_allow_localhost() {
+        $auth = new ZAuth();
+
         $this->reset_sockets();
         $this->server->set_zap_domain("global");
-        $this->auth->allow("127.0.0.1");
+        $auth->allow("127.0.0.1");
         $this->assertTrue($this->can_connect());
     }
 
     public function test_plain_no_auth() {
+        $auth = new ZAuth();
+
         // PLAIN
         $this->reset_sockets();
         $this->server->set_plain_server(true);
@@ -57,27 +65,33 @@ class ZAuthTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function test_plain_auth_ok() {
+        $auth = new ZAuth();
+
         // PLAIN WITH PW
         $this->reset_sockets();
         file_put_contents(static::cert_dir ."/passwords", "admin=password");
         $this->server->set_plain_server(true);
-        $this->auth->configure(ZAuth::AUTH_PLAIN, static::cert_dir ."/passwords");
+        $auth->configure(ZAuth::AUTH_PLAIN, static::cert_dir ."/passwords");
         $this->client->set_plain_username("admin");
         $this->client->set_plain_password("password");
         $this->assertTrue($this->can_connect());
     }
 
     public function test_plain_auth_error() {
+        $auth = new ZAuth();
+
         // PLAIN WITH PW ERROR
         $this->reset_sockets();
         $this->server->set_plain_server(true);
-        $this->auth->configure(ZAuth::AUTH_PLAIN, static::cert_dir ."/passwords");
+        $auth->configure(ZAuth::AUTH_PLAIN, static::cert_dir ."/passwords");
         $this->client->set_plain_username("admin");
         $this->client->set_plain_password("bogus");
         $this->assertFalse($this->can_connect());
     }
 
+    /*
     public function test_curve_auth_all() {
+        $auth = new ZAuth();
 
         if(ZSys::has_curve()) {
             $this->reset_sockets();
@@ -99,7 +113,7 @@ class ZAuthTest extends \PHPUnit_Framework_TestCase {
             $client_cert->apply($this->client);
             $this->server->set_curve_server(true);
             $this->client->set_curve_serverkey($server_key);
-            $this->auth->configure(ZAuth::AUTH_CURVE, "*");
+            $auth->configure(ZAuth::AUTH_CURVE, "*");
             $this->assertTrue($this->can_connect());
             $this->reset_sockets();
 
@@ -109,16 +123,16 @@ class ZAuthTest extends \PHPUnit_Framework_TestCase {
             $this->server->set_curve_server(true);
             $this->client->set_curve_serverkey($server_key);
             $client_cert->save_public(static::cert_dir ."/mycert.txt");
-            $this->auth->configure(ZAuth::AUTH_CURVE, static::cert_dir);
+            $auth->configure(ZAuth::AUTH_CURVE, static::cert_dir);
             $this->assertTrue($this->can_connect());
 
         }
 
     }
-*/
+
+    */
 
     private function reset_sockets() {
-        $this->auth = new ZAuth();
         $this->server = new ZSocket(ZSys::SOCKET_PUSH);
         $this->client = new ZSocket(ZSys::SOCKET_PULL);
         $this->client->set_rcvtimeo(500);
