954cd47497dd41e5773e2d20c7593ad63df3a351
Goddanao <goddanao@gmail.com>
2015-11-22 21:25:25 +0100

    fix Malamute test timeout

--- tests/MalamuteTest.php
+++ tests/MalamuteTest.php
@@ -77,7 +77,7 @@ class MalamuteTest extends \PHPUnit_Framework_TestCase {
             return ($msg_count == $processed) ? "OK" : "KO";
         });
 
-        sleep(4);
+        sleep(8);
 
         $client->receive();
         $result = $client->getResult();
@@ -111,7 +111,7 @@ class MalamuteTest extends \PHPUnit_Framework_TestCase {
             $service_client->send_mailbox('my_mailbox_worker', json_encode(['gino' => 'pino' . $i]), 1000);
         });
 
-        sleep(2);
+        sleep(5);
 
         $w->receive();
         $result = $w->getResult();
@@ -151,7 +151,7 @@ class MalamuteTest extends \PHPUnit_Framework_TestCase {
                 return $rcv == "mydata" ? "OK" : "KO";
             });
 
-        sleep(2);
+        sleep(5);
 
 
         $res = true;
